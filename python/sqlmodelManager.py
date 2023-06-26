5b64d180dfa36fb18f05cefcab890f69c92d096d
Owor Patrick Ikongha <powor288@gmail.com>
2023-06-26 10:05:01 +0100

    added env variable and jwt

--- python/sqlmodelManager.py
+++ python/sqlmodelManager.py
@@ -16,6 +16,63 @@ def createNew():
         os.makedirs(os.path.join(BASE_DIR, "migrations"))
     if not os.path.exists(os.path.join(BASE_DIR,"migrations", "versions")):
         os.makedirs(os.path.join(BASE_DIR,"migrations", "versions"))
+    with open(os.path.join(app_folder, "authentication.py"), "w") as authFile:
+        authFile.write('''import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+MINUTES = 30 
+
+class AuthHandler():
+    security = HTTPBearer()
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    secret = 'SECRET'
+
+    def get_password_hash(self, password):
+        return self.pwd_context.hash(password)
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def encode_token(self, user_id):
+        payload = {
+            'exp': datetime.utcnow() + timedelta(days=0, minutes=MINUTES),
+            'iat': datetime.utcnow(),
+            'sub': user_id
+        }
+        return jwt.encode(
+            payload,
+            self.secret,
+            algorithm='HS256'
+        )
+
+    def decode_token(self, token):
+        try:
+            payload = jwt.decode(token, self.secret, algorithms=['HS256'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise HTTPException(status_code=401, detail='Signature has expired')
+        except jwt.InvalidTokenError as e:
+            raise HTTPException(status_code=401, detail='Invalid token')
+
+    def auth_wrapper(self, auth: HTTPAuthorizationCredentials = Security(security)):
+        return self.decode_token(auth.credentials)
+    
+auth_handler = AuthHandler()        
+"""
+::: to encode a token  -> 
+token = auth_handler.encode_token(user['username'])
+
+::: jwt protected route
+def protected(username=Depends(auth_handler.auth_wrapper)):
+    return { 'name': username }
+"""
+''')
+
+    with open(os.path.join(BASE_DIR, ".env"), "w") as envFile:
+        envFile.write(f"""DATABASE_URL = {database_url}""")
 
     with open(os.path.join(app_folder, "models.py"), "w") as modelFile:
         modelFile.write("""from sqlmodel import SQLModel, Field
@@ -32,8 +89,9 @@ def createNew():
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-
-
+from sqlmodel import SQLModel
+from dotenv import load_dotenv
+load_dotenv()
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
@@ -63,7 +121,8 @@ app = FastAPI()
 
 @app.on_event("startup")
 async def on_startup():
-    init_db()       
+    pass
+    # init_db()       
 """)
 
 
